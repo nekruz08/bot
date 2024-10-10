eabfb21273b8232a425e314073392507c3db3101
NekruziHikmatullo <nekruzihikmatullo@gmail.com>
2024-10-10 11:29:33 +0500

    Move commands to separate package

---
+++ internal/app/commands/default.go
@@ -0,0 +1,13 @@
+package commands
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (c *Commander) Default(inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
+	c.bot.Send(msg)
+}
